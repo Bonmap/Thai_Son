3b6f530f37d356ab4ea4c47d3978330768c6b8d2
bonmap <son2nkt@gmail.com>
2023-10-09 15:47:15 +0700

    Test

---
+++ buoi 4/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+namespace buoi_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello world");
+            Console.ReadKey(); 
+        }
+    }
+}
