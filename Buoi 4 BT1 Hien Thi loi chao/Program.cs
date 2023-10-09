89112d97e4aa7253a2d554e94986cc4bc0464d2b
bonmap <son2nkt@gmail.com>
2023-10-10 00:48:16 +0700

    Cap nhat BT Hien thi loi chao

---
+++ Buoi 4 BT1 Hien Thi loi chao/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Text;
+
+namespace Buoi_4_BT1_Hien_Thi_loi_chao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            Console.WriteLine("Enter your name: ");
+            string yourName = Console.ReadLine();
+            Console.WriteLine("Hello: " + yourName);
+            Console.ReadKey();
+        }   
+    }
+}
