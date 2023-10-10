7d84d003b049e58b88c3967187ed0afbe30980be
bonmap <son2nkt@gmail.com>
2023-10-10 21:44:08 +0700

    Test thu

---
+++ Test/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sotuoi;
+            int songay=sotuoi*365;
+            sotuoi = int.Parse(Console.ReadLine());
+            if() 
+                
+           Console.ReadKey();
+        }
+    }
+}
