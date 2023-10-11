624e6dac045d895bd33f17413cbaffc88d875804
bonmap <son2nkt@gmail.com>
2023-10-11 12:52:51 +0700

    Cap nhat bai Su dung toan tu

---
+++ Buoi 4 TH2 Su dung toan tu/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Buoi_4_TH2_Su_dung_toan_tu
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.InputEncoding = Encoding.Unicode;
+            Console.OutputEncoding = Encoding.Unicode;
+            double chieu_dai;
+            double chieu_rong;
+            Console.WriteLine("Tính diện tích một hình chữ nhật");
+            Console.WriteLine("Vui lòng nhập chiều dài. Đơn vị là m");
+            chieu_dai = double.Parse(Console.ReadLine());
+            Console.WriteLine("Vui lòng nhập chiều rộng. Đơn vị là m");
+            chieu_rong = double.Parse(Console.ReadLine());
+            double dien_tich = chieu_dai * chieu_rong;
+            Console.WriteLine("Diện tích hình chữ nhật là "+ dien_tich+ " m2");
+            Console.ReadKey();
+        }
+    }
+}
